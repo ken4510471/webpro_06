0c4e29a93088ff4dbd503deab2016319294902d4
Ken Oyama <s24g1037at@s.chibakoudai.jp>
2025-10-24 17:41:19 +0900

    janken01.ejsを作成し，app5.jsに対応するプログラムを追記した．

--- app5.js
+++ app5.js
@@ -63,4 +63,47 @@ app.get("/janken", (req, res) => {
   res.render( 'janken', display );
 });
 
+app.get("/janken01", (req, res) => {
+  let hand = req.query.radio;
+  let win = Number( req.query.win );
+  let total = Number( req.query.total );
+  console.log( {hand, win, total});
+  const num = Math.floor( Math.random() * 3 + 1 );
+  let cpu = '';
+  let judgement = '';
+  if( num==1 ) cpu = 'グー';
+  else if( num==2 ) cpu = 'チョキ';
+  else cpu = 'パー';
+  // ここに勝敗の判定を入れる
+  // 以下の数行は人間の勝ちの場合の処理なので，
+  // 判定に沿ってあいこと負けの処理を追加する
+  if (hand==cpu) {judgement = "あいこ";}
+  else if(hand=="グー" && cpu=="チョキ")
+    {
+    judgement = "勝ち";
+    win += 1;
+  }
+  else if(hand=="チョキ" && cpu=="パー")
+    {
+    judgement = "勝ち";
+    win += 1;
+  }
+  else if(hand=="パー" && cpu=="グー")
+    {
+    judgement = "勝ち";
+    win += 1;
+  }
+  else {judgement = "負け";  
+  }
+  total += 1;
+  const display = {
+    your: hand,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  res.render( 'janken01', display );
+});
+
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
